39c41cd02f0042cfeabb1a25786d522fcefa2caa
Enrico <antoinegouiffes@gmail.com>
2021-11-23 09:20:03 +0100

    Safety_Protocol

---
+++ Automatic_Gate.ino
@@ -0,0 +1,76 @@
+// Portail automatique:Safety part
+
+// loop with open function 
+
+
+#define trigPin 13
+#define echoPin 12
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+// creation of the function tht will permit to detect movement below the gate
+int safety() { 
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  
+  if (distance >= 200 || distance <= 0)
+    {
+      //continue the closing protocol
+     
+    }
+  else {
+      // write a function to go to the opening function
+    } 
+  delay(500);
+}
+// must repeat the function as long as the gate is not fully closed. 
+int protocolSafety(){
+  
+}
+
+
+// must add a buzzer to warn of the closing procedure=part of the safety procedure
+
+
+#include "pitches.h"
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+void setup() {
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(8);
+  }
+}
+
+void loop() {
+  // no need to repeat the melody.
+}
