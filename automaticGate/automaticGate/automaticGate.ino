2dbae293861ca39c8a88f0b1a3487bbfc3d16fea
LamaNivelle <hectordetok@gmail.com>
2021-11-24 17:44:55 +0100

    mon progres

--- automaticGate/automaticGate/automaticGate.ino
+++ automaticGate/automaticGate/automaticGate.ino
@@ -6,7 +6,7 @@
 #define echoPin 12
 #define Password_Length 4
 
-///**********************************************************Hector
+//**********************************************************Hector
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
@@ -190,7 +190,7 @@ int noteDurations[] = {
 void setup() {
   Serial.begin(9600);
   
-  ///Hector
+  //Hector
   lcd.begin(16,2);
   
   //Agathe
